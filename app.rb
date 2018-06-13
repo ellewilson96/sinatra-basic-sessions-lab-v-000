0aefdcfa84f89a8a91af5572e35803818f4d1e4c
Elle Wilson <eleanorwilsonn@gmail.com>
2018-06-13 17:48:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,8 +12,8 @@ class App < Sinatra::Base
   end
 
   post '/checkout' do
-  @session << item[params]
-  @session[params]
+  session[:item] = params[:item]
+  @session = session
   erb :checkout
   end
 end
